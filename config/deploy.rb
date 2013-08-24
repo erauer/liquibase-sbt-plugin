aa4a7ad07b3027ae7a95ebb85c882c2f3ed95f21
Eric Rauer <eric.rauer@gmail.com>
2013-08-23 17:08:01 -0700

    Update deployment for QA

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,18 @@ task :production do
   server "seamobileapp04:2020", :app, :web
 end
 
-
+task :qa do
+  set :gateway, "msnap@63.236.65.21"
+  set :branch, (ENV['branch']||ENV['BRANCH']||"develop")
+  set :user, 'msnap'
+  set :deploy_to, "/var/www/apps/liquibase"
+  server "seamobweb01", :app, :web, :primary => true # web01
+  server "seamobweb02", :app, :web # web02
+  server "seamobweb03", :app, :web # web03
+  server "seamobweb04", :app, :web # web04
+  #server "10.10.0.52", :app, :web # wrk01
+  #server "10.10.0.55", :app, :web # wrk02
+end
 
 namespace :deploy do
   task :start do ; end
